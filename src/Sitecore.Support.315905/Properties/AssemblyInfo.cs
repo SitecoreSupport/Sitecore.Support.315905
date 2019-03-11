177509af6fcbeab61896df79e6dbca03db782b03
DK\dmsa <dmsa@sitecore.net>
2019-03-11 12:04:15 +0200

    Origin code of the "RadioButtonList.cshtml" file.

---
+++ src/Sitecore.Support.315905/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.315905")]
+[assembly: AssemblyProduct("Sitecore.Support.315905")]
+[assembly: ComVisible(false)]
